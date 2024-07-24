5cebc55ad0775bc8f56746b5f76158ad87e69265
arpan0001 <arpan21csu561@ncuindia.edu>
2024-07-24 17:29:04 +0530

    commit

--- Assets/BannerAds.cs
+++ Assets/BannerAds.cs
@@ -1,18 +1,60 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using GoogleMobileAds;
+using GoogleMobileAds.Api;
 
 public class BannerAds : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public void Start()
     {
-        
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize((InitializationStatus initStatus) =>
+        {
+            // This callback is called once the MobileAds SDK is initialized.
+        });
+        CreateBannerView();
     }
 
-    // Update is called once per frame
-    void Update()
+    
+  // These ad units are configured to always serve test ads.
+ #if UNITY_ANDROID
+  private string _adUnitId = "ca-app-pub-3954878672207532/9032753300";
+ #elif UNITY_IPHONE
+  private string _adUnitId = "ca-app-pub-3940256099942544/2934735716";
+ #else
+  private string _adUnitId = "unused";
+ #endif
+
+  BannerView _bannerView;
+
+  /// <summary>
+  /// Creates a 320x50 banner view at top of the screen.
+  /// </summary>
+  public void CreateBannerView()
+  {
+      Debug.Log("Creating banner view");
+
+      
+      // Create a 320x50 banner at top of the screen
+      _bannerView = new BannerView(_adUnitId, AdSize.Banner, AdPosition.Top);
+      LoadAd();
+  }
+
+  public void LoadAd()
+ {
+    // create an instance of a banner view first.
+    if(_bannerView == null)
     {
-        
+        CreateBannerView();
     }
+
+    // create our request used to load the ad.
+    var adRequest = new AdRequest();
+
+    // send the request to load the ad.
+    Debug.Log("Loading banner ad.");
+    _bannerView.LoadAd(adRequest);
+ }
+
 }
